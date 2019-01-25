3b83fed5eac2973326aaf8b593e135065b474b72
metchel <matthew.etchells@mail.mcgill.ca>
2019-01-24 21:01:04 -0500

    ...

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -8,7 +8,7 @@ print(most_frequent_words)
 X, y = process_features(data, most_frequent_words)
 train_X, train_y, validate_X, validate_y, test_X, test_y = train_validate_test_split(X, y)
 
-def least_squares_(X, y):
+def least_squares(X, y):
     return np.linalg.lstsq(X, y, rcond=None)
 print(least_squares(train_X, train_y))
 
